e1fd36bb59d5a710cd20f3397ec2a94022926f38
Waterboy1887 <connorw.cc@gmail.com>
2022-10-27 12:49:20 -0600

    created server

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>Hello, Node.js!</h1>");
+    response.end();
+}).listen(3000);
