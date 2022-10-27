7665c81fe42e960f4504895c35b161246128d5be
Waterboy1887 <connorw.cc@gmail.com>
2022-10-27 13:11:47 -0600

    added personal info

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require("http");
+const { REPL_MODE_SLOPPY } = require("repl");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<h1>Hello, Node.js!</h1>");
+    response.write("<h2>Connor Carlson - j79k878</h2>");
+    response.write("<p>Running node server on port 3016</p>")
     response.end();
 }).listen(3016);
