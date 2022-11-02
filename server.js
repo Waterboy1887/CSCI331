220da76a831bdd06d665ae1e7cbc5e3b91f31716
Waterboy1887 <connorw.cc@gmail.com>
2022-11-02 17:04:25 -0600

    Added link to personal GitHub repository

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ http.createServer(function(request, response) {
     response.write("<h1>Hello, Node.js!</h1>");
     response.write("<h2>Connor Carlson - j79k878</h2>");
     response.write("<p>Running node server on port 3016</p>");
-    response.write("<p>Link to my clas repository <a>https://github.com/Waterboy1887/CSCI331</a></p>");
+    response.write("<p>Link to my class repository <a href='https://github.com/Waterboy1887/CSCI331'>https://github.com/Waterboy1887/CSCI331</a></p>");
     response.end();
 }).listen(3016);
