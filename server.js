45e6347780db616383dbfbf5cb72ef80b557b68b
Waterboy1887 <connorw.cc@gmail.com>
2022-11-02 16:55:50 -0600

    Added link to personal GitHub repository

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<h1>Hello, Node.js!</h1>");
     response.write("<h2>Connor Carlson - j79k878</h2>");
-    response.write("<p>Running node server on port 3016</p>")
+    response.write("<p>Running node server on port 3016</p>");
+    response.write("<a>https://github.com/Waterboy1887/CSCI331</a>");
     response.end();
 }).listen(3016);
