ff234c1d7921fc8aa2b4a22c783c90b4f578d288
Waterboy1887 <connorw.cc@gmail.com>
2022-11-03 14:14:14 -0600

    Changed header

--- lab9/src/App.js
+++ lab9/src/App.js
@@ -4,7 +4,7 @@ import GitHub from './components/GitHub';
 function App() {
   return (
     <div>
-      <h1>Hello, Node.js!</h1>
+      <h1>Hello, React.js!</h1>
       <h2>Connor Carlson - j79k878</h2>
       <p>Running node server on port 3016</p><hr />
       <Counter incBy={1}/><hr />
